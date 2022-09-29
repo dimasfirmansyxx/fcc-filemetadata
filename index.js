edcf038bd792e9a4f81ede82ee9b3b7191fc627c
Dimas Nasution <dimasfirmansyxx@gmail.com>
2022-09-29 10:41:46 +0700

    formatting and install dependencies

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var cors = require('cors');
+var express = require('express')
+var cors = require('cors')
 require('dotenv').config()
 
-var app = express();
+var app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
   console.log('Your app is listening on port ' + port)
-});
+})
